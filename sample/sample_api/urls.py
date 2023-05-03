8f80f3d24a6c0c1a2042f1d27607071b757c6c3d
harish-crossml <harish.thakur@crossml.com>
2023-05-03 18:02:43 +0530

    added .gitignore file

--- sample/sample_api/urls.py
+++ sample/sample_api/urls.py
@@ -2,9 +2,19 @@ from django.conf import urls
 from django.urls import path
 from .views import *
 
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('', StudentView.as_view(), name='home'),
-    path('api/<str:id>/', StudentView.as_view(), name='index'),
+    path('student/<str:id>/', StudentView.as_view(), name='index'),
     #path('api/student/', StudentList.as_view(), name='students'),
     #path('api/students/<str:id>', StudentDetail.as_view(), name='students-detail'),
+
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
