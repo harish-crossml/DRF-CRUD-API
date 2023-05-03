9705b99e43a727538063923ed3c7793fc3070daa
harish-crossml <harish.thakur@crossml.com>
2023-05-03 12:01:48 +0530

    add comments in views.py

--- sample/sample_api/urls.py
+++ sample/sample_api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', StudentView.as_view(), name='home'),
-    # path('api/<str:id>/', StudentView.as_view(), name='index'),
-    path('api/student/', StudentList.as_view(), name='students'),
-    path('api/students/<str:id>', StudentDetail.as_view(), name='students-detail'),
+    path('', StudentView.as_view(), name='home'),
+    path('api/<str:id>/', StudentView.as_view(), name='index'),
+    #path('api/student/', StudentList.as_view(), name='students'),
+    #path('api/students/<str:id>', StudentDetail.as_view(), name='students-detail'),
 ]
--- sample/sample_api/views.py
+++ sample/sample_api/views.py
@@ -7,36 +7,37 @@ from .models import Students
 from .serializers import StudentSerializer  
 # Create your views here.  
 
-# class StudentView(APIView):
-#     def get(self, request):
-#         students = Students.objects.all()
-#         serializer = StudentSerializer(students, many=True)
-#         return Response(serializer.data)
+class StudentView(APIView):
+    def get(self, request):
+        students = Students.objects.all()    # Get all students
+        serializer = StudentSerializer(students, many=True)   # create instance & set many=True for multiple objects.
+        return Response(serializer.data)                      # return serialized data
 
-#     def post(self, request):
-#         serializer = StudentSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)   # create instance containing requested data
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def put(self, request, id):
-#         student = Students.objects.get(id=id)
-#         serializer = StudentSerializer(student, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, id):
+        student = Students.objects.get(id=id)    # get student with given id
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def delete(self, request, id):
-#         student = Students.objects.get(id=id)
-#         student.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, id):
+        student = Students.objects.get(id=id)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
-class StudentList(ListCreateAPIView):
-    queryset = Students.objects.all()
-    serializer_class = StudentSerializer
 
-class StudentDetail(RetrieveUpdateDestroyAPIView):
-    quesryset = Students.objects.all()
-    serializer_class = StudentSerializer
+# class StudentList(ListCreateAPIView):
+#     queryset = Students.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentDetail(RetrieveUpdateDestroyAPIView):
+#     quesryset = Students.objects.all()
+#     serializer_class = StudentSerializer
